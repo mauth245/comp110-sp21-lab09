800c9e948b922674e14ef6078ebb9eff7591a8fd
Marin Auth <mauth@sandiego.edu>
2021-03-23 17:00:54 -0700

    "completed lab 09"

--- comp110_lab09.py
+++ comp110_lab09.py
@@ -12,8 +12,26 @@ def get_grade_frequencies(filename):
     Creates a dictionary mapping letter grade to the number of students with
     that grade for the midterm exam #1.
     """
+    
+    grade_dict={"A":0,"B":0,"C":0,"D":0,"F":0} 
+    grades=open(filename, 'r')
+    grades.readline()
 
-    pass  # placeholder. Remove this line when you are done.
+    for line in grades:
+        values=line.split(",")
+        if 90<=int(values[5])<= 100:
+            grade_dict["A"] += 1
+        elif 80<=int(values[5])< 90:
+            grade_dict["B"] += 1
+        elif 70<=int(values[5])< 80:
+            grade_dict["C"] += 1
+        elif 60<=int(values[5])< 70:
+            grade_dict["D"] += 1
+        elif int(values[5])< 60:
+            grade_dict["F"] += 1
+
+    return grade_dict
+
 
 
 def test_get_grade_frequencies():
@@ -21,7 +39,7 @@ def test_get_grade_frequencies():
 
     # To Do: update the next line so it is a dictionary with the correct,
     # expected values.
-    expected = {}
+    expected = {"A":3,"B":6,"C":8,"D":2,"F":1}
 
     if actual == expected:
         print("Test PASSED")
@@ -43,9 +61,15 @@ def main():
 
     # To Do: Step 3: Update the labels for the x and y axis as well as the
     # title of the chart.
-    pp.xlabel("FIXME")
-    pp.ylabel("FIXME")
-    pp.title("FIXME")
+
+
+    import matplotlib.pyplot as pp
+    data = get_grade_frequencies("students.txt")
+    
+    pp.bar(data.keys(),data.values())
+    pp.xlabel("Letter Grade")
+    pp.ylabel("Number of Students")
+    pp.title("Midterm Grade Distribution")
     pp.show()
 
 
